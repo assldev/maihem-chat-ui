dae175654981ba6083d83c4d84b8d62902783979
akshitsingla <assl_dev@yahoo.com>
2025-02-16 20:36:41 +0000

    Implemented shadcn Login block. Login button is dummy - resirects to chat page

--- webapp/src/app/login/page.tsx
+++ webapp/src/app/login/page.tsx
@@ -1,7 +1,13 @@
+import { LoginForm } from "@/components/LoginForm";
+import Image from "next/image";
+
 export default function LoginPage() {
     return (
-      <div className="w-full h-full flex justify-center items-center">
-        LOGIN PAGE
+      <div className="w-full h-full flex flex-col justify-center items-center gap-4">
+        <Image src="/maihem-logo-transparent.png" alt="Maihem Logo" width={150} height={75} />
+        <div className="w-full max-w-sm">
+          <LoginForm />
+        </div>
       </div>
     );
   }
