9ec940679a1a87b819992ddc878d819745953d2b
akshitsingla <assl_dev@yahoo.com>
2025-02-16 22:13:53 +0000

    Implemented chat page layout, setup whatsapp frame

--- webapp/src/app/chat/page.tsx
+++ webapp/src/app/chat/page.tsx
@@ -1,7 +1,31 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function ChatPage() {
     return (
-      <div className="w-full h-full flex justify-center items-center">
-        CHAT PAGE
+      <div className="w-full h-full flex">
+        {/* SIDEBAR */}
+        <div className="h-full w-[60px]">
+          <Sidebar />
+        </div>
+
+        {/* LEFT PANEL */}
+        <div className="h-full w-[200px] bg-blue-200"></div>
+
+        {/* MAIN SECTION */}
+        <div className="h-full flex-grow p-10">
+          <div className="w-full h-full wa-frame pr-6 flex flex-col">
+            <div className="h-[85px] w-[50%] flex">
+              <div className="w-full bg-blue-100"></div>
+            </div>
+            <div className="flex-grow bg-red-100"></div>
+            <div className="h-[75px] flex justify-end">
+              <div className="w-[275px] mt-1 mb-7 bg-blue-100"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* RIGHT PANEL */}
+        <div className="h-full w-[200px] bg-blue-200"></div>
       </div>
     );
   }
---
+++ webapp/src/components/Sidebar.tsx
@@ -0,0 +1,89 @@
+import Image from "next/image";
+import { AvatarFallback, Avatar, AvatarImage } from "./ui/avatar";
+import AgentBubble from "./AgentBubble";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+
+const getAgentsListFromContext = () => {
+    return [
+        {
+            "id": "e4a4c8b2f9",
+            "name": 'Customer Service Bot',
+            "icon": 'MessageCircle'
+        }, {
+            "id": "7d1d5a3c9b",
+            "name": 'Mortgage Inquiry Bot',
+            "icon": 'House'
+        }, {
+            "id": "6b2c8a4f0e",
+            "name": 'Investment Advisory Bot',
+            "icon": 'TrendingUp'
+        }, {
+            "id": "9a3b5c7d1e",
+            "name": 'Fraud Prevention and Security Bot',
+            "icon": 'Shield'
+        }, {
+           "id": "2c5b8a1d9f",
+            "name": 'Premium Client Concierge Bot',
+            "icon": 'Star'
+        }
+    ];
+}
+
+const getActiveAgentIdFromContext = () => {
+    return "7d1d5a3c9b";
+}
+
+const getUserInfoFromContext = () => {
+    return {
+        "id": "8f4e2d6c0b",
+        "name": "Akshit Singla",
+        "email": "akshit@maihem.ai",
+        "initials": "AK",
+        "avatar": "https://github.com/shadcn.png"
+    }
+}
+
+export default function Sidebar() {
+
+    const agents = getAgentsListFromContext();
+    const activeAgentId = getActiveAgentIdFromContext();
+    const userInfo = getUserInfoFromContext();
+
+    return (
+        <div className="w-full h-full flex flex-col gap-8 px-1 bg-light-gray py-5">
+            {/* MAIHEM LOGO */}
+            <div className="w-full flex justify-center">
+                <Image src="/maihem-logo-only-transparent.png" alt="Maihem Logo" width={20} height={20} />
+            </div>
+            
+            {/* AGENTS LIST */}
+            <div className="flex-grow flex flex-col items-center gap-3">
+                <h1 className="text-xs text-muted-foreground font-bold text-center">AI AGENTS</h1>
+                {agents.map((agent) => (
+                    <AgentBubble name={agent.name} iconName={agent.icon} selected={activeAgentId == agent.id}/>
+                ))}
+            </div>
+            
+            {/* USER PILL */}
+            <div className="flex flex-col items-center">
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Avatar>
+                                <AvatarImage src={userInfo.avatar} alt={`Avatar for user: ${userInfo.name}`} />
+                                <AvatarFallback>{userInfo.initials}</AvatarFallback>
+                            </Avatar>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p className="text-sm">{`Logged In as:`}</p>
+                            <p className="font-bold">{userInfo.name}</p>
+                            <p className="italic">{userInfo.email}</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+                
+            </div>
+        </div>
+    );
+  }
+  
